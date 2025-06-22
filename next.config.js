760f37ec3f3bb992a358fe295393d281357cfb7f
rouven fisher <dira.berlin@gmail.com>
2025-06-22 15:48:08 +0200

    Improve rotating message font styling - use Segoe UI with medium weight and better spacing for customer-friendly appearance

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'assets.untappd.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   
