49c1d116cbb3559d5a8b30f51bf3fadba0265fc6
rouven fisher <dira.berlin@gmail.com>
2025-06-23 13:21:41 +0200

    Fix Wix embedding by removing conflicting X-Frame-Options header

--- next.config.js
+++ next.config.js
@@ -25,10 +25,6 @@ const nextConfig = {
         source: '/display/:path*',
         headers: [
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
             key: 'Content-Security-Policy',
             value: "frame-ancestors 'self' *.wix.com *.wixsite.com;",
           },
